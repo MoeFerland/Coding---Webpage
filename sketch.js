bda668a7f506c6af74adb2e6ddd9a9d8cf2096ad
MoeFerland <moe.ferland16@gmail.com>
2020-09-08 14:25:23 -0400

    pattern first attempt

--- sketch.js
+++ sketch.js
@@ -1,8 +1,32 @@
 function setup() {
   // put setup code here
-  background(100);
+createCanvas(700,700);
+background(200,10,120);
+
 }
 
 function draw() {
-  // put drawing code here
+  // teal ellipses
+  fill(0,200,200);
+
+  rotate(45,20);
+ellipse(120,-20,50,30);
+
+ellipse(200,-150,50,30);
+
+ellipse(280,-280,50,30);
+
+ellipse(360,-410,50,30);
+
+ rotate(135,20);
+
+ellipse(-220,-60,50,30);
+
+ellipse(-310,60,50,30);
+
+ellipse(-400,180,50,30);
+
+ellipse(-490,300,50,30);
+
+
 }
