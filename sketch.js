a597d3a9327662a0e8dbcb61808b7a9abf4d6234
MoeFerland <moe.ferland16@gmail.com>
2020-09-14 23:09:17 -0400

    background image attempt

--- sketch.js
+++ sketch.js
@@ -1,32 +1,12 @@
+var img;
 function setup() {
   // put setup code here
-createCanvas(700,700);
-background(200,10,120);
+  createCanvas(5000,5000);
+  img= loadImage('80spattern.jpg');
 
 }
 
 function draw() {
-  // teal ellipses
-  fill(0,200,200);
-
-  rotate(45,20);
-ellipse(120,-20,50,30);
-
-ellipse(200,-150,50,30);
-
-ellipse(280,-280,50,30);
-
-ellipse(360,-410,50,30);
-
- rotate(135,20);
-
-ellipse(-220,-60,50,30);
-
-ellipse(-310,60,50,30);
-
-ellipse(-400,180,50,30);
-
-ellipse(-490,300,50,30);
-
+  image(img,0,0);
 
 }
