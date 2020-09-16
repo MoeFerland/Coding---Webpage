cc91084b1a7aeccacb5ee45a7a723343b024a93f
MoeFerland <moe.ferland16@gmail.com>
2020-09-16 00:23:18 -0400

    text attempt

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
+let myFont;
+
+function preload(){
+  myFont = loadFont('43886.otf');
+}
+
 let img;
+
+
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
+
   img= loadImage('80spattern.jpg');
+
 
 }
 
 function draw() {
-  image(img,0,0,windowWidth,windowHeight);
+  image(img,0,0,1000,1000);
+
+    textFont('43886.otf');
+    textSize(60);
+    fill(205,92,92);
+    text('HELLO.',100,100);
 
 }
