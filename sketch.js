adc474c041ef46626e3911f389ef3db1d04c5015
MoeFerland <moe.ferland16@gmail.com>
2020-09-17 13:32:06 -0400

    ball Y movement

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ let flegreiReg;
   function preload() {
     flegreiReg = loadFont('43886.otf');
   }
+//ball movement
+let xpos = 0;
+let ypos = 0;
+let ballspeedY = 5
 
 function setup() {
   // put setup code here
@@ -14,15 +18,29 @@ function setup() {
 }
 
 function draw() {
-  image(img,0,0,1000,1000);
 
-    textFont(flegreiReg);
-    fill(205,92,92);
-    textSize(150);
-    text('HELL   .',80,300,);
+image(img,0,0,1000,1000);
 
-noStroke();
-circle(520,250,120);
+//HELLO
+textFont(flegreiReg);
 fill(205,92,92);
+textSize(150);
+text('HELL   .',80,300,);
+
+//ball
+noStroke();
+circle(xpos,ypos,120);
+fill(205,92,92);
+
+   ypos = ypos + ballspeedY;
+
+   if (ypos > height){
+     ballspeedY = - ballspeedY;
+   }
+
+   if (ypos < 0){
+     ballspeedY = - ballspeedY;
+   }
+
 
 }
