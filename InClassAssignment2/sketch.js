00198ca5be8e7781acda5df11c84ab3f10bca244
MoeFerland <moe.ferland16@gmail.com>
2020-09-21 15:00:08 -0400

    old sketch added to ICA 2

--- InClassAssignment2/sketch.js
+++ InClassAssignment2/sketch.js
@@ -1,15 +1,55 @@
+let img;
+let flegreiReg;
+  function preload() {
+    flegreiReg = loadFont('43886.otf');
+  }
+//ball movement
+let xpos = 120;
+let ypos = 0;
+let ballspeedY = 5;
+let ballspeedX = 6;
 
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
+  createCanvas(windowWidth,windowHeight);
+
+  img= loadImage('80spattern.jpg');
+
 
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
 
+image(img,0,0,1000,1000);
+
+//HELLO
+textFont(flegreiReg);
+fill(205,92,92);
+textSize(150);
+text('HELL   .',80,300,);
+
+//ball
+noStroke();
+circle(xpos,ypos,120);
+fill(205,92,92);
+
+   ypos = ypos + ballspeedY;
+   xpos = xpos + ballspeedX;
+
+   if (ypos > height){
+     ballspeedY = - ballspeedY;
+   }
+
+   if (ypos < 0){
+     ballspeedY = - ballspeedY;
+   }
+
+   if (xpos > width){
+     ballspeedX = - ballspeedX;
+   }
+
+   if (xpos < 0){
+     ballspeedX = - ballspeedX;
+   }
 
 }
