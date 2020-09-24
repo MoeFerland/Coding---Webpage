d95115f816f0bd27ad10515e06214087553f7fd8
MoeFerland <moe.ferland16@gmail.com>
2020-09-24 14:15:15 -0400

    ugh

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -4,15 +4,12 @@ function setup() {
 createCanvas(windowWidth,windowHeight);
 background(100,100,70);
 
-let flegreiReg;
-  function preload() {
-    flegreiReg = loadFont('43886 copy.otf');
-
 }
 
 function draw() {
   //Put draw code here
-for (let xpos = 0; xpos < random(0,width, xpos = xpos + 10)){
+for (let counter = 0; counter < width; counter = counter + 10){
   fill(0,0,255);
-  rect(xpos, xpos, 10);}
+  rect(counter,counter, 10);
+}
 }
