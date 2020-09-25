776751921ce47d5cfd5578de39688d286605b09f
MoeFerland <moe.ferland16@gmail.com>
2020-09-24 21:44:46 -0400

    R color attempt 4

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -9,8 +9,8 @@ background(0,0,255);
 function draw() {
   //Put draw code here
 for (let xpos = 0; xpos < width; xpos = xpos + 10){
-let rCol = 0;
-  fill(rCol + 5,50,50);
+let rCol = 0; rCol < 255; rCol = rCol + 5
+  fill(rCol,50,50);
   rect(xpos,xpos, 10);
 
 }
