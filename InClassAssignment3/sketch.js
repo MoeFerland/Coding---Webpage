655cc36f214fd796bbf660ac4e9fe691ebe6cad4
MoeFerland <moe.ferland16@gmail.com>
2020-09-30 00:11:11 -0400

    triangle adjustment

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -58,7 +58,7 @@ for (let cirNumber = 0; cirNumber < 1; cirNumber = cirNumber + 1){
 
 //triangle :3
   fill(255,36,0);
-  triangle(600,250,600,150,660,200);
+  triangle(600,230,600,150,660,190);
 }
 
 function draw() {
