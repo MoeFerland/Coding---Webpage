9ac7ec339b854454990b09f297b19101d5cce74f
MoeFerland <moe.ferland16@gmail.com>
2020-12-10 23:21:00 -0500

    adding final project 2

---
+++ Final2/sketch.js
@@ -0,0 +1,26 @@
+//loading font into sketch
+let narlyFONT;
+  function preload() {
+    narlyFONT = loadFont('NarlyOT_Outline_coding.otf');
+}
+
+let womxntext;
+  function preload(){
+   womxntext = loadImage('WOMXN-codingfinal.png');
+}
+
+
+
+function setup() {
+  // put setup code here
+createCanvas(1000, 2500);
+background(0,0,0);
+
+
+//womxn text iamge
+blendMode(SCREEN);
+tint(255,128);
+womxntext.resize(width,height);
+image(womxntext,0,0);
+
+}
