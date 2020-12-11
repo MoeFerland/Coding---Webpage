f6bc40992cb48b76ff4ee807b94ea97dbef3318a
MoeFerland <moe.ferland16@gmail.com>
2020-12-10 22:27:10 -0500

    text image attempt 1

--- Final/sketch.js
+++ Final/sketch.js
@@ -4,10 +4,10 @@ let narlyFONT;
     narlyFONT = loadFont('NarlyOT_Outline_coding.otf');
 }
 
-//let womxntext;
-//  function preload(){
-  //  womxntext = loadImage
-//  }
+let womxntext;
+  function preload(){
+   womxntext = loadImage('https://github.com/MoeFerland/Coding---Webpage/blob/69941579feb91ac0748240e0a4b6477b341b464c/Final/WOMXN-codingfinal.png');
+}
 
 
 
@@ -16,4 +16,8 @@ function setup() {
 createCanvas(1000, 2500);
 background(0,0,0);
 
+}
+
+function draw(){
+  image(womxntext,100,100);
 }
