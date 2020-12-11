67053e9bdb4114eae7d6e4e237b6e2117d2494c6
MoeFerland <moe.ferland16@gmail.com>
2020-12-11 14:43:37 -0500

    adding black text and motion

--- Final1/sketch.js
+++ Final1/sketch.js
@@ -4,13 +4,17 @@ let ypos = 1;
 let textspeedY = 3;
 let womxntext;
 let ladybody;
-
+let womxnBLK;
+let xloc;
+let yloc;
 
   function preload(){
     //'womxn'text
    womxntext = loadImage('WOMXN-codingfinal.png');
     //lady body image
    ladybody = loadImage('Ladybody1.png');
+    //'womxn' text black
+   womxnBLK = loadImage('WOMXN-codingfinalBLACK.png');
 }
 
 
@@ -31,9 +35,21 @@ function draw(){
 
   ypos = ypos + textspeedY;
 
-
-
-
 //ladybody image
 image(ladybody,0, 0, width, height);
+
+//'womxn' black text
+womxnBLK.resize(503,115);
+image(womxnBLK,)
+
+yloc = yloc = textspeedY;
+
+if (yloc > height - 115){
+  textspeedY = - textspeedY;
+}
+
+if (yloc = 0){
+  textspeedY = - textspeedY;
+}
+
 }
