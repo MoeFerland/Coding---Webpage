db29007736d06900dc3b68dbe5a362bdf3fe0641
MoeFerland <moe.ferland16@gmail.com>
2020-12-11 12:59:12 -0500

    text motion attempt

--- Final1/sketch.js
+++ Final1/sketch.js
@@ -3,24 +3,37 @@ let narlyFONT;
   function preload() {
     narlyFONT = loadFont('NarlyOT_Outline_coding.otf');
 }
-
+//"womxn" text
 let womxntext;
   function preload(){
    womxntext = loadImage('WOMXN-codingfinal.png');
 }
-
+let xpos = 0;
+let ypos = 0;
+let textspeedY = 3
 
 
 function setup() {
   // put setup code here
 createCanvas(515,800);
 background(0,0,0);
+}
 
 
-//womxn text iamge
-blendMode(SCREEN);
-tint(255,128);
-womxntext.resize(503,115);
-image(womxntext,0,0);
+function draw(){
+  //womxn text image
+  tint(255,128);
+  womxntext.resize(503,115);
+  image(womxntext,0,0);
 
+  xpos = xpos;
+  ypos = ypos + textspeedY;
+
+  if (ypos > height - 115){
+    textspeedY = -textspeedY;
+  }
+
+  if (ypos < 115){
+    textspeedY = - textspeedY;
+  }
 }
